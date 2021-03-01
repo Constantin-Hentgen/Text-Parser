ea230aa66595834dd3745733632932b579646a1e
Constantin <constantin.hentgen@protonmail.com>
2021-03-01 23:38:44 +0100

    premier commit

--- engine.py
+++ engine.py
@@ -1,2 +1,8 @@
 #utiliser un fichier remplis de français pour pouvoir y faire une analyse de fréquence des lettres
 #aussi pouvoir dire quel sont les mots les plus fréquents
+#faire l'intégralité du dev en POO
+
+f = open('hugo_notre_dame_de_paris.txt','r')
+lecture = f.read()
+print(lecture)
+f.close()
