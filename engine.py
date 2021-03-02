dcb362a1536ec7b99ea99648310ae5559a3b7892
Constantin <constantin.hentgen@protonmail.com>
2021-03-02 20:10:24 +0100

    ça fonctionne et il reste le ranking à produire

--- engine.py
+++ engine.py
@@ -1,55 +1,99 @@
-#utiliser un fichier remplis de français pour pouvoir y faire une analyse de fréquence des lettres
-#aussi pouvoir dire quel sont les mots les plus fréquents
-#faire l'intégralité du dev en POO
-#permettre l'exploration par mot et par phrase
-#permettre la lecture par ligne
+from random import randint
 
-f = open('hugo_notre_dame_de_paris.txt','r')
+# utiliser un fichier remplis de français pour pouvoir y faire une analyse de fréquence des lettres
+# aussi pouvoir dire quel sont les mots les plus fréquents
+# faire l'intégralité du dev en POO
+# permettre l'exploration par mot et par phrase
+# permettre la lecture par ligne
+
+f = open("hugo_notre_dame_de_paris.txt", "r")
 lecture = f.read()
 f.close()
-
-#si il y a un espace c'est un nouveau mot je lis, dès que c'est un espace j'ajoute les caractères
-
-#faire une fonction pour afficher un arrondi super propre à lire
-
-#variable = 0.12345
-#variable = str(variable) faire attention puisque le point est considéré comme un caratère
-#print(variable[3])
-
-def arrondi(number):
-    number = str(number)
-    product = ""
-    iteration = 0
-    test = 0
-
-    while iteration < 2:
-        if number[test] != "0" and number[test] != ".":
-            iteration += 1
-
-        product += number[test]
-        test += 1
-
-    return product,iteration,test
-
-print("fonction arrondi pour number = 0.0000000323123456 : ", arrondi(0.000000032312346))
-
-#il va de chiffre en chiffre, il lui faut 3 chiffres non nuls . compris
-
-
-
 
 def frequency(element):
     element = str(element)
     total = 0
     number = 0
+    space = 0
 
     for letter in lecture:
         total += 1
 
         if letter == element:
             number += 1
-    
-    return number*100/total, "%"
 
-#element = input("Enter a character : ")
-#print(frequency(element))
+        elif letter == " ":
+            space += 1
+
+    return number * 100 / total, space * 100 / total, total / space
+
+
+# element = input("Enter a character : ")
+# print(frequency(element))
+
+
+def split():
+    split = []
+    iterator = 0
+    iteration = 0
+
+    for a in lecture:
+        if (
+            a == " "
+            or a == "."
+            or a == ","
+            or a == "\n"
+            or a == " "
+            or a == ""
+            or a == "–"
+            or a == "…"
+            or a == "’"
+            or a == ":"
+            or a == "»"
+            or a == "«"
+            or a == "-"
+            or a == "0"
+            or a == "1"
+            or a == "2"
+            or a == "3"
+            or a == "4"
+            or a == "5"
+            or a == "6"
+            or a == "7"
+            or a == "8"
+            or a == "9"
+        ):
+            if lecture[iterator:iteration] != "":
+                split.append(lecture[iterator:iteration])
+            iterator = iteration + 1
+
+        iteration += 1
+
+    return split
+
+
+# split = split()
+split = ["bonsoir", "je", "suis", "bonsoir", "bonjour"]
+
+
+def ranker():
+    rank = []
+    count = 0
+
+    for a in range(len(split)):
+        for b in range(len(split)):
+            if split[b] == split[a]:
+                count += 1
+        rank.append(count)
+        count = 0
+
+    return rank
+
+#faire un top 50 des mots les plus utilisés par hugo
+
+rank = ranker()
+
+# print(split[randint(0,len(split))])
+
+print(split)
+print(rank)
