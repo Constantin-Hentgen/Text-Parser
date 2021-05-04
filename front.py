09e87f67e919145ad500f76725d729f135dbd006
Constantin <constantin.hentgen@protonmail.com>
2021-05-04 16:20:15 +0200

    fenêtre popup

--- front.py
+++ front.py
@@ -2,15 +2,28 @@ from random import randint
 from tkinter import *
 from tkinter import filedialog
 import os
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+import tkinter.ttk as ttk
+from ttkthemes import ThemedStyle
+
 
 window = Tk()
+style = ThemedStyle(window)
+style.set_theme("equilux")
+
+
+main = ttk.Frame(window)
+main.pack(side=TOP,fill=X,expand=True)
 
 window.title("Text Analyzer by Constantin")
 window.geometry('1900x1080')
 #window.attributes('-fullscreen', True)
 
 display_text = StringVar()
-lbl = Label(window, textvariable=display_text)
+lbl = ttk.Label(window, textvariable=display_text)
 #lbl.grid(column=1,row=1)
 lbl.place(relx=0.5, rely=0.4, anchor=CENTER)
 
@@ -50,8 +63,8 @@ def phrase_counter(original):
 
     return compteur_points
 
-def frequency(original): #renvoi la fréquence pour chaque caractère
-    ranking = [] #liste vide pour contenir les fréquences
+def frequency(original): #renvoi la Quantité pour chaque caractère
+    ranking = [] #liste vide pour contenir les Quantité
     total = 0
     number = 0
 
@@ -64,15 +77,15 @@ def frequency(original): #renvoi la fréquence pour chaque caractère
             if element == letter:
                 number += 1 #incrémenter pour la stat de la lettre par occurence
 
-        number = number * 100 / total #calcul de la fréquence
+        #number = number * 100 / total #calcul de la Quantité
         ranking.append(number)
 
         number = 0 #réinitialisation de number pour pas qu'une lettre hérite les statistiques d'une autre
 
-    return ranking #ranking des fréquences selon l'ordre alphabétique
+    return ranking #ranking des Quantité selon l'ordre alphabétique
 
-def frequenci(original): #renvoi la fréquence pour chaque caractère
-    ranking = [] #liste vide pour contenir les fréquences
+def frequenci(original): #renvoi la Quantité pour chaque caractère
+    ranking = [] #liste vide pour contenir les Quantité
     total = 0
     number = 0
 
@@ -85,12 +98,12 @@ def frequenci(original): #renvoi la fréquence pour chaque caractère
             if element == letter:
                 number += 1 #incrémenter pour la stat de la lettre par occurence
 
-        number = number * 100 / total #calcul de la fréquence
+        number = number * 100 / total #calcul de la Quantité
         ranking.append(number)
 
         number = 0 #réinitialisation de number pour pas qu'une lettre hérite les statistiques d'une autre
 
-    return ranking #ranking des fréquences selon l'ordre alphabétique
+    return ranking #ranking des Quantité selon l'ordre alphabétique
 
 def split(document): #sépare le textes en petites unités que nous allons analyser
     split = [] #liste résultat contenant les unités séparées
@@ -167,7 +180,7 @@ def vowel(document):
   
     return compteur_voyelles, compteur_consonnes, compteur_consonnes+compteur_voyelles,lettres_francais_accents
 
-def algotri(liste_rank, real_liste): #trie la liste des unités en fonction de leur fréquence
+def algotri(liste_rank, real_liste): #trie la liste des unités en fonction de leur Quantité
     for a in range(len(liste_rank)):
         maximum = 0
         rank = 0
@@ -184,8 +197,12 @@ def algotri(liste_rank, real_liste): #trie la liste des unités en fonction de l
 #############################################################################################
 
 def rocket():
-    #original = identity(pathfinder())
-    original = identity("/home/constantin/github/text-analyzer/fasten.txt")
+    #path = pathfinder()
+    #original = identity(path)
+    original = identity("/home/constantin/github/text-analyzer/fastfr.txt")
+    window = Tk()
+    window.title("path")
+    window.geometry('1900x2000')
 
     compteur_lettres = 0
 
@@ -201,7 +218,7 @@ def rocket():
     taux_voyelles = int(1000000*vowel(original)[0]/vowel(original)[2])/10000
     taux_consonnes = int(1000000*vowel(original)[1]/vowel(original)[2])/10000
     liste_lettre = algotri(frequency(original), list(alphabet))[1] # liste triée dans l'ordre décroissant d'occurences des lettres de l'alphabet
-    lettre_rang = algotri(frequency(original), list(alphabet))[0] # liste triée dans l'ordre décroissant d'occurences des fréquences des lettres de l'alphabet
+    lettre_rang = algotri(frequency(original), list(alphabet))[0] # liste triée dans l'ordre décroissant d'occurences des Quantité des lettres de l'alphabet
 
     s = display_text.get()
     s += "\n Les mots ont en moyenne " + str(lettre_par_mot) + " lettres."
@@ -215,10 +232,7 @@ def rocket():
     else:
         s += "\n Consonnes majoritaires ( "+ str(taux_consonnes) +" % )"
 
-    s += "\n ratio de voyelles accentuées : "+str(int(1000000*vowel(original)[3]/vowel(original)[2])/10000)+"%"
-    s += "\n Les phrases contiennent "+str(mot_par_phrase)+" mots en moyenne."
-    s += "\n Les mots ont en moyenne "+str(lettre_par_mot)+" lettres."
-    s += "\n proportion de mots uniques : "+str(mot_uniques_sur_total)+" %"
+    s += "\n ratio de voyelles accentuées : "+str(int(1000000*vowel(original)[3]/vowel(original)[2])/10000)+"% \n Les phrases contiennent "+str(mot_par_phrase)+" mots en moyenne. \n Les mots ont en moyenne "+str(lettre_par_mot)+" lettres. \n proportion de mots uniques : "+str(mot_uniques_sur_total)+" %"
     #print(len(liste)/len(split(original)))
     #print(len(split(original))/compteur_points)
     if mot_par_phrase >= 10 and mot_par_phrase <= 30 and nombre_total_mots >= 150:
@@ -230,6 +244,76 @@ def rocket():
 
     display_text.set(s)
 
+    solution = [lettre_rang,liste_lettre,rang,liste]
+
+    counter = 0
+    while solution[0][counter] == 0:
+        del solution[0][counter]
+        del solution[1][counter]
+
+    raffin_rang = []
+    raffin_liste = []
+
+    for a in range(20):
+        raffin_liste.append(solution[3][-a-1])
+        raffin_rang.append(solution[2][-a-1])
+
+    #print(raffin_rang,raffin_liste)
+
+    data1 = {'Lettres': solution[1],
+             'Quantité': solution[0]
+            }
+    df1 = DataFrame(data1,columns=['Lettres','Quantité'])
+
+
+
+    data2 = {'Mots': raffin_liste,
+             'Quantité': raffin_rang
+            }
+    df2 = DataFrame(
+        {"Quantité":raffin_rang},
+        index=raffin_liste)
+    
+    frame = Frame(window)
+    frame.pack(side=TOP,fill=X,expand=True)
+    
+    fram = Frame(window)
+    fram.pack(side=BOTTOM,fill=X,expand=True)
+
+    figure1 = plt.Figure(dpi=100)
+    ax1 = figure1.add_subplot(111)
+    ax1.scatter(df1['Lettres'],df1['Quantité'], color = 'r')
+    scatter1 = FigureCanvasTkAgg(figure1, frame)
+    scatter1.get_tk_widget().pack(side=LEFT, fill=BOTH)
+    ax1.legend(['Quantité']) 
+    ax1.set_xlabel('Lettres')
+    ax1.set_title("Nombre d'apparitions des lettres")
+
+    figure2 = plt.Figure(dpi=100)
+    ax2 = figure2.add_subplot(111)
+    bar2 = FigureCanvasTkAgg(figure2, frame)
+    bar2.get_tk_widget().pack(side=LEFT, fill=BOTH,expand=True)
+    #df2 = df2[['Mots','Quantité']].groupby('Mots').sum()
+    df2.plot(kind='barh', legend=True, ax=ax2)
+    ax2.set_title("Nombre d'apparitions des mots")
+
+    figure1 = plt.Figure(dpi=100)
+    ax1 = figure1.add_subplot(111)
+    ax1.scatter(df1['Lettres'],df1['Quantité'], color = 'r')
+    scatter1 = FigureCanvasTkAgg(figure1, fram)
+    scatter1.get_tk_widget().pack(side=LEFT, fill=BOTH)
+    ax1.legend(['Quantité']) 
+    ax1.set_xlabel('Lettres')
+    ax1.set_title("Nombre d'apparitions des lettres")
+
+    figure2 = plt.Figure(dpi=100)
+    ax2 = figure2.add_subplot(111)
+    bar2 = FigureCanvasTkAgg(figure2, fram)
+    bar2.get_tk_widget().pack(side=LEFT, fill=BOTH,expand=True)
+    #df2 = df2[['Mots','Quantité']].groupby('Mots').sum()
+    df2.plot(kind='barh', legend=True, ax=ax2)
+    ax2.set_title("Nombre d'apparitions des mots")
+
     return lettre_rang,liste_lettre,rang,liste
 
 #btn = Button(window, text="Get path", command=pathfinder)
@@ -237,85 +321,24 @@ def rocket():
 
 #print(rocket()[0],rocket()[1])
 
-from pandas import DataFrame
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-
-solution = rocket()
-counter = 0
-while solution[0][counter] == 0:
-    del solution[0][counter]
-    del solution[1][counter]
-
-raffin_rang = []
-raffin_liste = []
-
-for a in range(20):
-    raffin_liste.append(solution[3][-a-1])
-    raffin_rang.append(solution[2][-a-1])
-
-#print(raffin_rang,raffin_liste)
-
-data1 = {'Lettres': solution[1],
-         'Fréquences': solution[0]
-        }
-df1 = DataFrame(data1,columns=['Lettres','Fréquences'])
 
 
-
-data2 = {'Mots': raffin_liste,
-         'Nb': raffin_rang
-        }
-df2 = DataFrame(
-    {"Nb":raffin_rang},
-    index=raffin_liste)
-
-#data2 = {'Year': [1920,1930,1940,1950,1960,1970,1980,1990,2000,2010],
-#         'Unemployment_Rate': [9.8,12,8,7.2,6.9,7,6.5,6.2,5.5,6.3]
-#        }
-#df2 = DataFrame(data2,columns=['Year','Unemployment_Rate'])
-#
-#
-#data3 = {'Interest_Rate': [5,5.5,6,5.5,5.25,6.5,7,8,7.5,8.5],
-#         'Stock_Index_Price': [1500,1520,1525,1523,1515,1540,1545,1560,1555,1565]
-#        }  
-#df3 = DataFrame(data3,columns=['Interest_Rate','Stock_Index_Price'])
-# 
-#####################################
-
-figure1 = plt.Figure(figsize=(6,5), dpi=100)
-ax1 = figure1.add_subplot(111)
-ax1.scatter(df1['Lettres'],df1['Fréquences'], color = 'b')
-scatter1 = FigureCanvasTkAgg(figure1, window)
-scatter1.get_tk_widget().pack(side=LEFT, fill=BOTH)
-ax1.legend(['Fréquences']) 
-ax1.set_xlabel('Lettres')
-ax1.set_title('Fréquence en fonction des lettres (%)')
-
-
-#figure2 = plt.Figure(figsize=(6,5), dpi=100)
+#figure2 = plt.Figure(dpi=100)
 #ax2 = figure2.add_subplot(111)
-#ax2.scatter(df2['Mots'],df2['Nb'], color = 'r')
+#ax2.scatter(df2['Mots'],df2['Quantité'], color = 'r')
 #scatter2 = FigureCanvasTkAgg(figure2, window)
-#scatter2.get_tk_widget().pack(side=RIGHT, fill=BOTH)
+#scatter2.get_tk_widget().pack(side=LEFT, fill=BOTH)
 #ax2.legend(["Nombre d'apparitions"])
 #ax2.set_xlabel('Mots')
 #ax2.set_title("Mots les plus courants")
 
 
-figure2 = plt.Figure(figsize=(6,5), dpi=100)
-ax2 = figure2.add_subplot(111)
-bar2 = FigureCanvasTkAgg(figure2, window)
-bar2.get_tk_widget().pack(side=RIGHT, fill=BOTH)
-#df2 = df2[['Mots','Nb']].groupby('Mots').sum()
-df2.plot(kind='barh', legend=True, ax=ax2)
-ax2.set_title("Mots les plus courants")
 
-#figure2 = plt.Figure(figsize=(6,5), dpi=100)
+#figure2 = plt.Figure(dpi=100)
 #ax2 = figure2.add_subplot(111)
 #bar2 = FigureCanvasTkAgg(figure2, window)
-#bar2.get_tk_widget().pack(side=RIGHT, fill=BOTH)
-#df2 = df2.plot(x="Mots",y=["Nb"],kind="barh")
+#bar2.get_tk_widget().pack(side=LEFT, fill=BOTH)
+#df2 = df2.plot(x="Mots",y=["Quantité"],kind="barh")
 #plt.show()
 ##df2.plot(kind='bar', legend=True, ax=ax2)
 #ax2.set_title("Nombre d'apparitions des mots")
@@ -348,9 +371,10 @@ ax2.set_title("Mots les plus courants")
 #
 #plt.show()
 ################################################################################
+rocket()
 
-#batn = Button(window, text="launch engine", command=rocket)
-##batn.grid(column=1,row=0)
-#batn.place(relx=0.5, rely=0.2, anchor=CENTER)
+batn = ttk.Button(window, text="launch engine", command=rocket)
+#batn.grid(column=1,row=0)
+batn.place(relx=0.5, rely=0.2, anchor=CENTER)
 
 window.mainloop()
